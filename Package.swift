411a6e617c6882646cb07b6440a8ce6e7f8c288f
Mattt <mattt@me.com>
2022-10-06 06:47:50 -0700

    Support code generation of models from OpenAPI descriptions (#7)
    * Add generate-replicate-model target
    
    * Add generate-replicate-model target to package products
    
    * Add iOS 15 to package platforms
    
    * Update README

--- Package.swift
+++ Package.swift
@@ -5,16 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "Replicate",
-    platforms: [.macOS(.v12)],
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Replicate",
             targets: ["Replicate"]),
+        .executable(
+            name: "generate-replicate-model",
+            targets: ["generate-replicate-model"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Flight-School/AnyCodable", .upToNextMinor(from: "0.6.6"))
+        .package(url: "https://github.com/Flight-School/AnyCodable", .upToNextMinor(from: "0.6.6")),
+        .package(url: "https://github.com/mattt/OpenAPIKit", revision: "86e2a2a762efae13b482ad64b2abe416a7c85cd8"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.1.4")),
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "0.50700.00"),
+        .package(url: "https://github.com/apple/swift-format.git", exact: "0.50700.00"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,14 +32,23 @@ let package = Package(
         .target(
             name: "Replicate",
             dependencies: [
-                .productItem(name: "AnyCodable", package: "AnyCodable", moduleAliases: nil, condition: nil)
+                .product(name: "AnyCodable", package: "AnyCodable"),
             ]
         ),
+        .executableTarget(
+            name: "generate-replicate-model",
+            dependencies: [
+                "Replicate",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
+                .product(name: "SwiftFormat", package: "swift-format"),
+                .product(name: "OpenAPIKit", package: "OpenAPIKit")
+            ]),
         .testTarget(
             name: "ReplicateTests",
             dependencies: [
-                .productItem(name: "AnyCodable", package: "AnyCodable", moduleAliases: nil, condition: nil),
-                .targetItem(name: "Replicate", condition: nil)
+                "Replicate",
+                .product(name: "AnyCodable", package: "AnyCodable"),
             ]),
     ]
 )
---
+++ Sources/generate-replicate-model/Extensions/FileHandle+Extensions.swift
@@ -0,0 +1,8 @@
+import class Foundation.FileHandle
+
+extension FileHandle: TextOutputStream {
+    public func write(_ string: String) {
+        guard let data = string.data(using: .utf8) else { return }
+        write(data)
+    }
+}
