eb48562bcbc6861e5a6b1476a86e5ac938aaa4d2
Mattt Zmuda <mattt@replicate.com>
2023-11-02 11:53:31 -0700

    Update hardware list

--- Tests/ReplicateTests/Helpers/MockURLProtocol.swift
+++ Tests/ReplicateTests/Helpers/MockURLProtocol.swift
@@ -165,10 +165,7 @@ class MockURLProtocol: URLProtocol {
                       { "name": "CPU", "sku": "cpu" },
                       { "name": "Nvidia T4 GPU", "sku": "gpu-t4" },
                       { "name": "Nvidia A40 GPU", "sku": "gpu-a40-small" },
-                      { "name": "Nvidia A40 (Large) GPU", "sku": "gpu-a40-large" },
-                      { "name": "Nvidia A40 (Large) GPU (8x)", "sku": "gpu-a40-large-8x" },
-                      { "name": "Nvidia A100 (40GB) GPU", "sku": "gpu-a100-small" },
-                      { "name": "Nvidia A100 (80GB) GPU", "sku": "gpu-a100-large" },
+                      { "name": "Nvidia A40 (Large) GPU", "sku": "gpu-a40-large" }
                     ]
                 """#
             case ("GET", "https://api.replicate.com/v1/models"?):
